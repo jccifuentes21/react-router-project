8526aebd5e2df79d4eae1daf3705c66c824669a7
Juan Camilo <jccifuentes21@gmail.com>
2022-08-26 09:24:20 -0700

    project finished!

--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,26 +1,62 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from "react";
 
-import classes from './NewCommentForm.module.css';
+import useHttp from "../../hooks/use-http";
+import { addComment } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
+
+import classes from "./NewCommentForm.module.css";
 
 const NewCommentForm = (props) => {
+  const [isEmpty, setIsEmpty] = useState(false);
   const commentTextRef = useRef();
+  const { sendRequest, status, error } = useHttp(addComment);
+  const { onAddedComment } = props;
+
+  useEffect(() => {
+    if (status === "completed" && !error) {
+      onAddedComment();
+    }
+  }, [status, error, onAddedComment]);
 
   const submitFormHandler = (event) => {
     event.preventDefault();
+    const enteredText = commentTextRef.current.value;
 
-    // optional: Could validate here
+    if (enteredText.trim().length === 0) {
+      setIsEmpty(true);
+    } else {
+      setIsEmpty(false);
+    }
 
-    // send comment to server
+    if (!isEmpty) {
+      // send comment to server
+      sendRequest({
+        commentData: { text: enteredText },
+        quoteId: props.quoteId,
+      });
+      commentTextRef.current.value= ''
+    }
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === "pending" && (
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      )}
       <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>Your Comment</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
+        <label htmlFor="comment">Your Comment</label>
+        <textarea
+          id="comment"
+          rows="5"
+          ref={commentTextRef}
+          required
+        ></textarea>
       </div>
+      {isEmpty && <p>Can't post an empty comment!</p>}
       <div className={classes.actions}>
-        <button className='btn'>Add Comment</button>
+        <button className="btn">Add Comment</button>
       </div>
     </form>
   );
